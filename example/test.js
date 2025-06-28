cd7f73f58b0b2968bd3c1b21f4c62c493885910b
Thomas Riffard <ferdodo@hotmail.fr>
2025-06-28 08:05:15 +0200

    Attempt testing with posix paths

--- example/test.js
+++ example/test.js
@@ -3,12 +3,12 @@
 import { $ } from "zx";
 import { rename } from "fs/promises";
 
-const result = await $`exportcase check src`;
+const result = await $`exportcase check ./src`;
 console.log(result.stdout);
 await rename("./src/greet.ts", "./src/salutation.ts");
 
 try {
-    await $`exportcase check src`;
+    await $`exportcase check ./src`;
     process.exit(1);
 } catch (err) {
     process.exit(0);
